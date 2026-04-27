65ff2198757ace95b967249689f1004d203471bc
safyah11 <safeah1424@gmail.com>
2026-04-27 22:45:44 +0300

    Update main message to indicate main branch version

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("--- Java Utility Tool v1.0 ---");
-        System.out.println("This is a simple SCM project.");
+        System.out.println("Main Branch Version");
     }
 }
