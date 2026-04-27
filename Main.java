6a6ccc141148d814801a6b8383c4b24f7359659f
safyah11 <safeah1424@gmail.com>
2026-04-27 22:39:34 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--- Java Utility Tool v1.0 ---");
+        System.out.println("This is a simple SCM project.");
+    }
+}
