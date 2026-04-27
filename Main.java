22ec966bb23932e2f32061ab30c3a78184f7e7ec
safyah11 <safeah1424@gmail.com>
2026-04-27 22:43:34 +0300

    Add addition feature

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("--- Java Utility Tool v1.0 ---");
-        System.out.println("This is a simple SCM project.");
+        System.out.println("--- Java Utility Tool: Calculator ---");
+        int a = 10, b = 5;
+        System.out.println("Sum: " + (a + b));
     }
 }
