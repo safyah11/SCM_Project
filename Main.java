860335890677658994038cec1135755aea3204cf
safyah11 <safeah1424@gmail.com>
2026-04-27 22:44:08 +0300

    Add temperature converter feature

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("--- Java Utility Tool v1.0 ---");
-        System.out.println("This is a simple SCM project.");
+        System.out.println("--- Java Utility Tool: Converter ---");
+        double celsius = 25;
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(celsius + "C is " + fahrenheit + "F");
     }
 }
